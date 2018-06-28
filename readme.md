a97047170a28932cd996629ded520db2639a9a32
Brendan Murty <b@murty.io>
2018-06-28 21:15:49 +1000

    Initial commit

---
+++ readme.md
@@ -0,0 +1,16 @@
+## Custom New Tab for Chrome
+
+A simple extension for Chrome to replace the New Tab page.
+
+Loads website tiles stored in a public online JSON file so that settings can sync between machines.
+
+[![Screenshot](https://raw.githubusercontent.com/brendanmurty/chrome-new-tab/master/screenshot.png)](screenshot.png)
+
+### Setup
+
+1. Create a new copy of `example-sites.json` and add your own sites
+2. Upload this file to your own web server and note the URL
+3. Clone the repository
+4. Update the URL to the JSON file by editing the `sites_json_file` variable in `script.js`
+5. Add the same URL to the `permissions` section in `manifest.json`
+5. Load the folder as an Unpacked Extension in Chrome by visiting `chrome://extensions`
---
+++ script.js
@@ -0,0 +1,26 @@
+var sites_json_file = 'http://json-schema.org/example/card.json';
+
+var xhr = new XMLHttpRequest();
+
+xhr.onload = function() {
+    var response = xhr.responseText;
+    json = JSON.parse(response);
+
+    for (var i = 0; i < json.length; i++) {
+        var site = json[i],
+            list_item = document.createElement('li'),
+            list_item_link = document.createElement('a'),
+            list_item_content = site.short_title;
+
+        list_item_link.title = site.title;
+        list_item_link.href = site.url;
+        list_item_link.style = 'background-color:' + site.colour_background + ';color:' + site.colour_foreground + ';';
+
+        list_item_link.appendChild(document.createTextNode(list_item_content));
+        list_item.appendChild(list_item_link);
+        document.getElementById('sites').appendChild(list_item);
+    }
+};
+
+xhr.open('GET', sites_json_file);
+xhr.send();
